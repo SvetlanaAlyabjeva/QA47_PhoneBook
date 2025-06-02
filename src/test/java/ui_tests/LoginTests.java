6ef0ddfc5d067baaf45751cf371495cdcef7684f
lanaal <svetlana.alyabjeva@gmail.com>
2025-06-02 16:09:28 +0300

    Login test

---
+++ src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,17 @@
+package ui_tests;
+
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends ApplicationManager {
+
+    @Test
+    public void loginPositiveTest(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm("svetlana.alyabjeva@gmail.com", "Sveta08@");
+    }
+}
